b0218fe4238bd861c0f99f91d22253612219f5c3
Marcelo Marcos <marcelomarcos.g9@gmail.com>
2024-08-03 00:22:33 -0300

    Aula de lógica em JS finalizada

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-alert(`Boas vindas ao jogo do número secreto!`);
+alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 10;
+let numeroMaximo = 300;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-let chute = prompt(`Escolha um número entre 1 e 10`);
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
 
-if(chute == numeroSecreto){
-    console.log(`Parabéns você acertou, o número é o ${numeroSecreto}`);
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
